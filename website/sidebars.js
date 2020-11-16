afcc61b29dbb24502dd1a29edbf127d427cf09d5
Michael Sena <michaelrsena@gmail.com>
2020-11-16 11:00:25 -0400

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   docs: {
     'Introduction': ['idx-welcome', 'idx-ecosystem'],
-    'Core Concepts': ['core-concepts-dids', 'core-concept-did-providers', 'core-concepts-index', 'core-concepts-definitions', 'core-concepts-references', 'core-concepts-schemas', 'core-concepts-ceramic'] 
+    'Core Concepts': ['core-concepts-dids', 'core-concepts-index', 'core-concepts-definitions', 'core-concepts-references', 'core-concepts-schemas', 'core-concepts-ceramic'] 
     'IDX libraries': ['libs-getting-started', 'libs-types', 'libs-tools', 'libs-idx', 'libs-web'],
     Guides: ['guide-cli', 'guide-public-data', 'guide-authentication', 'guide-definitions']
   }
