dc842c20069f6d4d1b3025f0b14bfd7aad503315
Elizabeth Murray <elizabeth@3box.io>
2021-11-30 13:03:57 -0500

    pre merge

--- packages/cli/test/commands/stream.test.ts
+++ packages/cli/test/commands/stream.test.ts
@@ -1,19 +1,12 @@
-import { PassThrough } from 'stream'
+import { expect, test } from '@oclif/test'
+// require('debug').enable('*')
 
-describe('streams', () => {
-  let spy = jest.spyOn(PassThrough, 'prototype', 'get')
+describe('streams', async () => {
+  test
+    .stderr()
+    .command(['stream:create', ''])
+    .it('does not create a stream & returns an informative error', async (ctx) => {
+      console.log('CTX: ', ctx.stderr)
+      expect(ctx.stderr).to.contain('Error: Invalid argument spec:')
+    })
 })
-
-// IF THE ABOVE FAILS RETURN TO OCLIF TESTING & MOCK:
-// https://github.com/sindresorhus/ora/blob/main/test.js#L13
-// import { expect, test } from '@oclif/test'
-
-// // require('debug').enable('*')
-// describe('stream', () => {
-//   test
-//     .stdout()
-//     .command(['did:create'])
-//     .it('errors', (ctx) => {
-//       expect(ctx.stdout).to.contain('Created DID')
-//     })
-// })
