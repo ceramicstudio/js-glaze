7995f8171a66d465b89edaef037c3ee86bff034d
Elizabeth Murray <elizabeth@3box.io>
2021-11-30 08:48:59 -0500

    test

--- packages/cli/test/commands/stream.test.ts
+++ packages/cli/test/commands/stream.test.ts
@@ -1,12 +1,19 @@
-import { expect, test } from '@oclif/test'
+import { PassThrough } from 'stream'
 
-import { Command as cmd } from '../../src/command'
-
-describe('stream', () => {
-  test
-    .stdout()
-    .do(() => cmd.run([]))
-    .it('displays a help message', (ctx) => {
-      expect(ctx.stdout).to.contain('Usage:')
-    })
+describe('streams', () => {
+  let spy = jest.spyOn(PassThrough, 'prototype', 'get')
 })
+
+// IF THE ABOVE FAILS RETURN TO OCLIF TESTING & MOCK:
+// https://github.com/sindresorhus/ora/blob/main/test.js#L13
+// import { expect, test } from '@oclif/test'
+
+// // require('debug').enable('*')
+// describe('stream', () => {
+//   test
+//     .stdout()
+//     .command(['did:create'])
+//     .it('errors', (ctx) => {
+//       expect(ctx.stdout).to.contain('Created DID')
+//     })
+// })
