86ae1b83723db4ff62b79eaba492647793e12bc8
Paul Le Cam <paul@3box.io>
2022-03-25 10:58:51 +0000

    Increase Ceramic daemon launch timeout for CLI tests

--- packages/cli/globalSetup.js
+++ packages/cli/globalSetup.js
@@ -4,7 +4,7 @@ export default async function globalSetup() {
   await setup({
     command: 'ceramic daemon --network inmemory',
     debug: true,
-    launchTimeout: 30000,
+    launchTimeout: 60000,
     port: 7007,
   })
 }
