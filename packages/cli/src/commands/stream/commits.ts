ecdb1955a8e369d17714424d3d66ecb6f3aee118
Artur Wdowiarski <arturwdowiarski@gmail.com>
2022-05-09 15:58:56 +0100

    Exposed sync options in stream:commits and stream:state cli commands

--- packages/cli/src/commands/stream/commits.ts
+++ packages/cli/src/commands/stream/commits.ts
@@ -1,22 +1,36 @@
-import { Command, type CommandFlags } from '../../command.js'
+import { SyncOptions } from '@ceramicnetwork/common'
+import { 
+  Command, 
+  STREAM_ID_ARG,
+  SYNC_OPTION_FLAG,
+  type CommandFlags
+} from '../../command.js'
 
-export default class Commits extends Command<CommandFlags, { streamId: string }> {
+type Flags = CommandFlags & {
+  syncOption?: SyncOptions
+}
+
+export default class Commits extends Command<Flags, { streamId: string }> {
   static description = 'list commits contained within a stream'
 
   static args = [
-    {
-      name: 'streamId',
-      description: 'ID of the stream',
-      required: true,
-    },
+    STREAM_ID_ARG,
   ]
+
+  static flags = {
+    ...Command.flags,
+    syncOption: SYNC_OPTION_FLAG,
+  }
 
   async run(): Promise<void> {
     this.spinner.start(`Loading stream ${this.args.streamId}...`)
     try {
-      const stream = await this.ceramic.loadStream(this.args.streamId)
+      const stream = await this.ceramic.loadStream(
+        this.args.streamId,
+        { sync: this.flags.syncOption }
+      )
       const commits = stream.allCommitIds.map((v) => v.toString())
-      this.spinner.succeed('Stream commits loaded.')
+      this.spinner.succeed(`Stream commits loaded.`)
       this.logJSON(commits)
     } catch (e) {
       this.spinner.fail((e as Error).message)
--- packages/cli/src/commands/stream/state.ts
+++ packages/cli/src/commands/stream/state.ts
@@ -1,22 +1,36 @@
-import { Command, type CommandFlags } from '../../command.js'
+import { SyncOptions } from '@ceramicnetwork/common'
+import { 
+  Command,
+  STREAM_ID_ARG,
+  SYNC_OPTION_FLAG,
+  type CommandFlags
+} from '../../command.js'
 
-export default class State extends Command<CommandFlags, { streamId: string }> {
+type Flags = CommandFlags & {
+  syncOption?: SyncOptions
+}
+
+export default class State extends Command<Flags, { streamId: string }> {
   static description = 'get the state of a Stream'
 
   static args = [
-    {
-      name: 'streamId',
-      required: true,
-      description: 'ID of the Stream',
-    },
+    STREAM_ID_ARG,
   ]
+
+  static flags = {
+    ...Command.flags,
+    syncOption: SYNC_OPTION_FLAG,
+  }
 
   async run(): Promise<void> {
     this.spinner.start(`Querying stream ${this.args.streamId}`)
 
     try {
-      const stream = await this.ceramic.loadStream(this.args.streamId)
-      this.spinner.succeed(`Successfully queried stream ${this.args.streamId}`)
+      const stream = await this.ceramic.loadStream(
+        this.args.streamId,
+        { sync: this.flags.syncOption }
+      )
+      this.spinner.succeed(`Successfully queried stream ${this.args.streamId}.`)
       this.logJSON(stream.state)
     } catch (e) {
       this.spinner.fail((e as Error).message)
