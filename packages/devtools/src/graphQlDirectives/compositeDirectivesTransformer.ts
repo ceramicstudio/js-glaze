8d1bd8dddd4e9c3bf1f7b4b51cb81b247449729a
Artur Wdowiarski <arturwdowiarski@me.com>
2022-05-13 15:51:35 +0100

    Fist proof-of-concept version of the compositeDefinitionFromSchema(...) function
    Added compositeDirectivesTransformer

---
+++ packages/devtools/src/graphQlDirectives/compositeDirectivesTransformer.ts
@@ -0,0 +1,67 @@
+import {
+  mapSchema,
+  getDirective,
+  MapperKind 
+} from '@graphql-tools/utils';
+import { 
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLFieldConfig
+} from 'graphql';
+
+function objectConfigMapperFactory(
+  schema: GraphQLSchema
+): (objectConfig: GraphQLObjectType<any, any>) => GraphQLObjectType<any, any> {
+  function objectConfigMapper(
+    objectConfig: GraphQLObjectType<any, any>
+  ) : GraphQLObjectType<any, any> {
+    const modelDirective = getDirective(schema, objectConfig, 'model')?.[0];
+      if (modelDirective) {
+        objectConfig.extensions = {
+          ...objectConfig.extensions,
+          ceramicExtensions: [
+            {
+              ceramicDirectiveName: "model",
+              accountRelation: modelDirective['index'].toLowerCase(),
+              modelDescription: modelDirective['description'],
+            }
+          ]
+        }
+      }
+      return objectConfig;
+  }
+  return objectConfigMapper
+}
+
+function fieldConfigMapperFactory(
+  schema: GraphQLSchema
+): (fieldConfig: GraphQLFieldConfig<any, any, any>) => GraphQLFieldConfig<any, any, any> {
+  function fieldConfigMapper(
+    fieldConfig: GraphQLFieldConfig<any, any, any>
+  ) : GraphQLFieldConfig<any, any, any> {
+    const lengthDirective = getDirective(schema, fieldConfig, 'length')?.[0];
+      if (lengthDirective) {
+        fieldConfig.extensions = {
+          ...fieldConfig.extensions,
+          ceramicExtensions: {
+            length: {
+              ceramicDirectiveName: "length",
+              min: lengthDirective.min,
+              max: lengthDirective.max,
+            }
+          }
+        }
+      }
+      return fieldConfig;
+  }
+  return fieldConfigMapper
+}
+
+export function compositeDirectivesTransformer(
+  schema: GraphQLSchema
+): GraphQLSchema {
+  return  mapSchema(schema, {
+    [MapperKind.OBJECT_TYPE]: objectConfigMapperFactory(schema),
+    [MapperKind.OBJECT_FIELD]: fieldConfigMapperFactory(schema),
+  });
+}
