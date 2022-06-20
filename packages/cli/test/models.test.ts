63cc070e02c2ae4b23f9ddfac1f61cdb3a8128a4
Artur Wdowiarski <arturwdowiarski@me.com>
2022-06-20 11:52:13 +0200

    Added tests for model:content

--- packages/cli/test/models.test.ts
+++ packages/cli/test/models.test.ts
@@ -31,9 +31,41 @@ describe('models', () => {
         MY_MODEL_JSON,
         `--key=${seed}`,
       ])
-      console.log(create.stderr.toString())
-
       expect(create.stderr.toString().includes('Created MyModel with streamID')).toBe(true)
+    }, 60000)
+  })
+
+  describe('model:content', () => {
+    test('model content display fails without the streamID', async () => {
+      await expect(execa('glaze', ['model:content'])).rejects.toThrow(
+        /streamId {2}ID of the stream/
+      )
+    }, 60000)
+
+    test('model content display succeeds', async () => {
+      const key = await execa('glaze', ['did:create'])
+      const seed = stripAnsi(key.stderr.toString().split('with seed ')[1])
+
+      const create = await execa('glaze', [
+        'model:create',
+        MY_MODEL_JSON,
+        `--key=${seed}`,
+      ])
+
+      const content = await execa('glaze', [
+        `model:content`,
+        create.stderr.toString().split('with streamID ')[1].replace('.', ''),
+        `--sync=sync-always`,
+      ])
+      const lines = stripAnsi(content.stderr.toString())
+      expect(lines.includes('"name": "MyModel"')).toBe(true)
+      expect(lines.includes('"schema": {')).toBe(true)
+      expect(lines.includes('"type": "object",')).toBe(true)
+      expect(lines.includes('"properties": {')).toBe(true)
+      expect(lines.includes('"accountRelation": "list"')).toBe(true)
+      expect(lines.includes('"$schema": "https://json-schema.org/draft/2020-12/schema",')).toBe(
+        true
+      )
     }, 60000)
   })
 })
