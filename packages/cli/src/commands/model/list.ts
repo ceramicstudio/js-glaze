0af262079e170a838687b0b1e5585d29bdf43d5e
Artur Wdowiarski <arturwdowiarski@me.com>
2022-06-30 10:53:35 +0100

    A working version of model:list command without pagination or tests

--- packages/cli/src/commands/model/list.ts
+++ packages/cli/src/commands/model/list.ts
@@ -1,5 +1,14 @@
 import { Command, type QueryCommandFlags } from '../../command.js'
 import { Flags } from '@oclif/core'
+import { Model } from '@ceramicnetwork/stream-model'
+import Table from 'cli-table3'
+import { StreamID } from '@ceramicnetwork/streamid'
+
+type PartialModelDefinition = {
+  id: StreamID
+  name: string
+  description: string
+}
 
 type ModelListFlags = QueryCommandFlags & {
   table?: boolean
@@ -17,22 +26,31 @@ export default class ModelList extends Command<ModelListFlags> {
 
   async run(): Promise<void> {
     try {
-      // const results = await this.ceramic.index.queryIndex({
-      //   first: 1000,
-      //   model: Model.MODEL,
-      // })
-      // const modelDefinitions = buildModelDefinitionsFromStreamStates(results.entries)
-      //
-      // if (this.flags.table === true) {
-      //   const table = new Table({
-      //     head: ['Name', 'ID', 'Alias', 'Description'],
-      //     colWidths: [32, 65, 32, 100],
-      //   })
-      //
-      // } else {
-      //   console.log(JSON.stringify(modelDefinitions))
-      // }
-      return Promise.resolve()
+      const page = await this.ceramic.index.queryIndex({
+        first: 1000,
+        model: Model.MODEL,
+      })
+      const fieldsToDisplay: Array<PartialModelDefinition> = page.edges.map((edge) => {
+        const stream = this.ceramic.buildStreamFromState(edge.node)
+        return {
+          id: stream.id,
+          name: (stream.content as Record<string, any>).name,
+          description: (stream.content as Record<string, any>).description,
+        }
+      })
+      if (this.flags.table === true) {
+        const table = new Table({
+          head: ['Name', 'ID', 'Description'],
+          colWidths: [32, 65, 100],
+        })
+        fieldsToDisplay.forEach((field) => {
+          table.push([field.name, field.id.toString(), field.description])
+        })
+        // Not using the spinner here, so that the table is laid out properly
+        this.log(table.toString())
+      } else {
+        console.log(JSON.stringify(fieldsToDisplay))
+      }
     } catch (e) {
       this.spinner.fail((e as Error).message)
       return
