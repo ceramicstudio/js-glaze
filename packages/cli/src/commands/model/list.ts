52bc2b81fb772049cb00f78cca9b65869edfb4da
Artur Wdowiarski <arturwdowiarski@me.com>
2022-07-04 12:21:40 +0100

    Simplistic version of pagination for models:list

--- packages/cli/src/commands/model/list.ts
+++ packages/cli/src/commands/model/list.ts
@@ -1,11 +1,12 @@
 import { Command, type QueryCommandFlags } from '../../command.js'
 import { Flags } from '@oclif/core'
+import { CliUx } from '@oclif/core'
 import { Model } from '@ceramicnetwork/stream-model'
 import Table from 'cli-table3'
-import { StreamID } from '@ceramicnetwork/streamid'
+import { Page, StreamState } from '@ceramicnetwork/common'
 
 type PartialModelDefinition = {
-  id: StreamID
+  idString: string
   name: string
   description: string
 }
@@ -15,6 +16,10 @@ type ModelListFlags = QueryCommandFlags & {
 }
 
 export default class ModelList extends Command<ModelListFlags> {
+  PAGE_SIZE = 3
+
+  lastLoadedPage: Page<StreamState> | null = null
+
   static description = 'load a list of models with optional pagination'
 
   static flags = {
@@ -26,34 +31,50 @@ export default class ModelList extends Command<ModelListFlags> {
 
   async run(): Promise<void> {
     try {
-      const page = await this.ceramic.index.queryIndex({
-        first: 1000,
+      this.lastLoadedPage = await this.ceramic.index.queryIndex({
+        first: this.PAGE_SIZE,
         model: Model.MODEL,
       })
-      const fieldsToDisplay: Array<PartialModelDefinition> = page.edges.map((edge) => {
-        const stream = this.ceramic.buildStreamFromState(edge.node)
-        return {
-          id: stream.id,
-          name: (stream.content as Record<string, any>).name,
-          description: (stream.content as Record<string, any>).description,
-        }
-      })
-      if (this.flags.table === true) {
-        const table = new Table({
-          head: ['Name', 'ID', 'Description'],
-          colWidths: [32, 65, 100],
+
+      this.displayPage(this.lastLoadedPage)
+
+      while (this.lastLoadedPage.pageInfo.hasNextPage) {
+        await CliUx.ux.anykey('Press any key to load the next page')
+        this.lastLoadedPage = await this.ceramic.index.queryIndex({
+          first: this.PAGE_SIZE,
+          model: Model.MODEL,
+          after: this.lastLoadedPage?.pageInfo.endCursor,
         })
-        fieldsToDisplay.forEach((field) => {
-          table.push([field.name, field.id.toString(), field.description])
-        })
-        // Not using the spinner here, so that the table is laid out properly
-        this.log(table.toString())
-      } else {
-        console.log(JSON.stringify(fieldsToDisplay))
+        this.displayPage(this.lastLoadedPage)
       }
     } catch (e) {
       this.spinner.fail((e as Error).message)
       return
+    }
+  }
+
+  displayPage(page: Page<StreamState>): void {
+    const fields: Array<PartialModelDefinition> = page.edges.map((edge) => {
+      const stream = this.ceramic.buildStreamFromState(edge.node)
+      return {
+        idString: stream.id.toString(),
+        name: (stream.content as Record<string, any>).name,
+        description: (stream.content as Record<string, any>).description,
+      }
+    })
+
+    if (this.flags.table === true) {
+      const table = new Table({
+        head: ['Name', 'ID', 'Description'],
+        colWidths: [32, 65, 100],
+      })
+      fields.forEach((field) => {
+        table.push([field.name, field.idString.toString(), field.description])
+      })
+      // Not using the spinner here, so that the table is laid out properly
+      this.log(table.toString())
+    } else {
+      this.log(JSON.stringify(fields))
     }
   }
 }
