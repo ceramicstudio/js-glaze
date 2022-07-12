04800d0d394ba55154d2e1e2c7342d5c3c2bfdcd
Artur Wdowiarski <arturwdowiarski@me.com>
2022-07-12 16:29:40 +0100

    Extract constants out of the function body

--- packages/cli/globalSetup.js
+++ packages/cli/globalSetup.js
@@ -2,37 +2,37 @@ import { setup } from 'jest-dev-server'
 import { Model } from '@ceramicnetwork/stream-model'
 import fs from 'fs-extra';
 
+const CWD = new URL(`file://${process.cwd()}/`);
+const TEST_DIR_PATH = new URL('test/', CWD)
+const CONFIG_DIR_PATH = new URL('config/', TEST_DIR_PATH)
+const CONFIG_PATH = new URL('daemon.config.json', CONFIG_DIR_PATH)
+const STATE_STORE_DIRECTORY = new URL('statestore/', TEST_DIR_PATH)
+const INDEXING_DB_FILENAME = new URL('indexing.sqlite', CONFIG_DIR_PATH)
+
+// Create the custom config here to make sure that we start with a clear indexing db setup each time
+// TODO: Investigate why the indexing API throws errors when we run tests several times without cleaning the db
+const TEST_DAEMON_CONFIG = {
+  anchor: {},
+  'http-api': { 'cors-allowed-origins': [new RegExp('.*')] },
+  ipfs: { mode: 'bundled' },
+  logger: { 'log-level': 2, 'log-to-files': false },
+  metrics: {
+    'metrics-exporter-enabled': false,
+    'metrics-port': 9090,
+  },
+  network: { name: 'testnet-clay' },
+  node: {},
+  'state-store': {
+    mode: 'fs',
+    'local-directory': STATE_STORE_DIRECTORY.pathname,
+  },
+  indexing: {
+    db: `sqlite://${INDEXING_DB_FILENAME.pathname}`,
+    models: [Model.MODEL.toString()],
+  },
+}
+
 export default async function globalSetup() {
-  const CWD = new URL(`file://${process.cwd()}/`);
-  const TEST_DIR_PATH = new URL('test/', CWD)
-  const CONFIG_DIR_PATH = new URL('config/', TEST_DIR_PATH)
-  const CONFIG_PATH = new URL('daemon.config.json', CONFIG_DIR_PATH)
-  const STATE_STORE_DIRECTORY = new URL('statestore/', TEST_DIR_PATH)
-  const INDEXING_DB_FILENAME = new URL('indexing.sqlite', CONFIG_DIR_PATH)
-
-  // Create the custom config here to make sure that we start with a clear indexing db setup each time
-  // TODO: Investigate why the indexing API throws errors when we run tests several times without cleaning the db
-  const TEST_DAEMON_CONFIG = {
-    anchor: {},
-    'http-api': { 'cors-allowed-origins': [new RegExp('.*')] },
-    ipfs: { mode: 'bundled' },
-    logger: { 'log-level': 2, 'log-to-files': false },
-    metrics: {
-      'metrics-exporter-enabled': false,
-      'metrics-port': 9090,
-    },
-    network: { name: 'testnet-clay' },
-    node: {},
-    'state-store': {
-      mode: 'fs',
-      'local-directory': STATE_STORE_DIRECTORY.pathname,
-    },
-    indexing: {
-      db: `sqlite://${INDEXING_DB_FILENAME.pathname}`,
-      models: [Model.MODEL.toString()],
-    },
-  }
-
   await fs.ensureDir(CONFIG_DIR_PATH)
   await fs.writeJson(CONFIG_PATH, TEST_DAEMON_CONFIG)
 
